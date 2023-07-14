8cc57f47c871bc07401681471e9fd4c4f1f81ea8
jamal eddine benrahmoun <benrahmounjamaleddine@gmail.com>
2023-07-14 16:41:29 -0700

    avoiding int

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * Description:  prints all the digit numbers of base 10,
+ * starting from 0 and followed by a new line,
+ * Return: 0
+ */
+int main(void)
+{
+	int c = '0';
+
+	while (c <= '9')
+	{
+		putchar(c);
+		c++;
+	}
+
+	putchar('\n');
+	return (0);
+}
