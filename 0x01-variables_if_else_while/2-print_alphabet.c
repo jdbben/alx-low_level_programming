ebea0b8342743286b1b604a89cc83998c8920389
jamal eddine benrahmoun <benrahmounjamaleddine@gmail.com>
2023-07-14 10:03:01 -0700

    using for loop

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+
+char alphabet[24] = "abcdefghijklmnopqrstuvwxyz";
+for (int i = 0 ; i <= 24 ; i++)
+{
+putchar(alphabet[i]);
+}
+return (0);
+}
