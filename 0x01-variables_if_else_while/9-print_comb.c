346217e219d0e2465bac65386ac9bcb2aa3017ac
jamal eddine benrahmoun <benrahmounjamaleddine@gmail.com>
2023-07-14 17:45:56 -0700

    done

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * Description:  prints all possible combinations of single-digit numbers,
+ * Return: 0
+ */
+int main(void)
+{
+	int c = '0';
+	while (c <= '9')
+	{
+		putchar(c);
+		if (c != 9)
+		{
+			putchar(' ');
+			putchar(',);
+		}
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
