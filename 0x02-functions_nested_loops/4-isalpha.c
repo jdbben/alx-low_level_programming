0afcd61d58b5f99ecabf2e1e0e054dcc46c3e807
jamal eddine benrahmoun <benrahmounjamaleddine@gmail.com>
2023-07-18 17:49:37 -0700

    isalphabet

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,19 @@
+#include <ctype.h>
+
+/**
+ * _isalpha - checks for alphabets
+ * @c: the character to check
+ * Return: 1 if c is alphabet, 0 otherwise
+ */
+
+int int _isalpha(int c)
+{
+if (isalpha(c))
+{
+	return (1);
+}
+else
+{
+	return (0);
+}
+}
