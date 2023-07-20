08670acd7b9867acb1410ec050786ab4caf58414
jamal eddine benrahmoun <benrahmounjamaleddine@gmail.com>
2023-07-20 09:08:41 -0700

    multiplaying

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+
+/**
+ * mul - multiplay 2 numbers
+ * @a: the first numbers to multiplay
+ * @b: the second numbers
+ * Return: multi
+ */
+
+int mul(int a, int b)
+{
+	multi = a * b;
+	return (multi);
+}
