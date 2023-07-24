492eb6024f242e55493492c199b9150390d94314
jamal eddine benrahmoun <benrahmounjamaleddine@gmail.com>
2023-07-24 14:35:46 -0700

    using puts as a solution

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _puts- prints a string, followed by a new line, to stdout.
+ *
+ * @str: the string to print.
+ * Return: 0.
+ */
+
+int _puts(char *str)
+{
+	puts(s);
+
+	return (0);
+}
