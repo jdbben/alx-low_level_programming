d9e875c2fb596ea2b750f73bd7702c692983b96b
jamal eddine benrahmoun <benrahmounjamaleddine@gmail.com>
2023-07-24 15:49:28 -0700

    using putchar

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,4 +1,6 @@
 #include "main.h"
+#include <stdio.h>
+#include <string.h>
 
 /**
  * print_rev - print a string in reverse.
@@ -12,9 +14,7 @@ void print_rev(char *s)
 	int i = 0;
 	int length;
 
-	for (length = 0; s[length] != '\0'; length++)
-	{
-	}
+	length = strlen(s);
 
 	for (i = length - 1; i >= 0; i--)
 	{
