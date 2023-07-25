2c5a1a968419ced952a4961167f65bf9abd2ff08
jamal eddine benrahmoun <benrahmounjamaleddine@gmail.com>
2023-07-24 17:16:15 -0700

    done

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -6,9 +6,10 @@
  * *_strcpy - copies the string pointed to by src, including \0.
  *@src: pointer.
  *@dest: pointer.
+ *Return: the pointer to dest.
  */
 
 char *_strcpy(char *dest, char *src)
 {
-	return (dest = strcpy(dest,src));
+	return (dest = strcpy(dest, src));
 }
