dde60f38dcdfa88d02a3a279b36fa8cc4180f14f
jamal eddine benrahmoun <benrahmounjamaleddine@gmail.com>
2023-07-26 14:20:53 -0700

    first commitment of the day

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,14 @@
+#include <string.h>
+
+/**
+ * *_strcat - take 2 parameters and concatenates them .
+ * @dest: parameter 1 the destnation.
+ * @src: the source file.
+ *
+ * 
+ */
+ 
+ char *_strcat(char *dest, char *src)
+{
+	strcat(src, dest);
+}
