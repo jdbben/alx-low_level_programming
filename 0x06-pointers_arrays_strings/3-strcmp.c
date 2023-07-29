5a60bb890c446def7c961d39645902892931229d
jamal eddine benrahmoun <benrahmounjamaleddine@gmail.com>
2023-07-29 06:20:07 -0700

    using strcmp function

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,14 @@
+#include <string.h>
+#include <stdio.h>
+
+/**
+ * _strcmp - compares two strings.
+ * @s1: string number 1
+ * @s2: string 2
+ * Return: Always 0 .
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	strcmp(s1, s2);
+}
