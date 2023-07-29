ff683b01d05d19b3e54783809d049471004367ff
jamal eddine benrahmoun <benrahmounjamaleddine@gmail.com>
2023-07-29 04:04:25 -0700

    beeing late

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -4,10 +4,11 @@
  **_strcat - take 2 parameters and concatenates them .
  *@dest: parameter 1 the destnation.
  *@src: the source file.
- *
+ * Return: This return to dest, that concatenates two strings
  */
 
 char *_strcat(char *dest, char *src)
 {
 	strcat(src, dest);
+	return (dest);
 }
