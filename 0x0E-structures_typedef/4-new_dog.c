e9c376e19d93b7252a31f8662e715f331d6e3d67
jamal eddine benrahmoun <benrahmounjamaleddine@gmail.com>
2023-08-15 05:05:10 -0700

    in every fail return NULL

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -11,12 +11,37 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-dog_t *user;
-user = malloc(sizeof(struct dog));
-if (user == NULL)
+unsigned int nl, ol, i;
+dog_t *dog;
+
+if (name == NULL || owner == NULL)
 return (NULL);
-user->name = name;
-user->age = age;
-user->owner = owner;
-return (user);
+dog = malloc(sizeof(dog_t));
+if (dog == NULL)
+return (NULL);
+for (nl = 0; name[nl]; nl++)
+;
+nl++;
+dog->name = malloc(nl *sizeof(char));
+if (dog->name == NULL)
+{
+free(dog);
+return (NULL);
+}
+for (i = 0; i < nl; i++)
+dog->name[i] = name[i];
+dog->age = age;
+for (ol = 0; owner[ol]; ol++)
+;
+ol++;
+dog->owner = malloc(ol *sizeof(char));
+if (dog->owner == NULL)
+{
+free(dog->name);
+free(dog);
+return (NULL);
+																					}
+for (i = 0; i < ol; i++)
+dog->owner[i] = owner[i];
+return (dog);
 }
